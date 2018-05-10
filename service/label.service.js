434e27906d5a8207a660dedf2c9bfb86c12c4cee
siddheshkadam1881 <siddheshkadam111@gmail.com>
2018-05-10 12:17:35 +0530

    working on service

--- service/label.service.js
+++ service/label.service.js
@@ -5,6 +5,12 @@ var User = require("../model/User");
  function Label()
  { }
 
+ /**
+   * @description createUserlabel function to create label..
+   * @class  createUserlabel
+   * @extends {labelObj,userObj,callback}
+   */
+
 
  Label.prototype.createUserlabel = function (labelObj,userObj,callback) {
    //labelModel.createUserlabel(labelObj,userObj,callback);
@@ -14,15 +20,36 @@ var User = require("../model/User");
    label.save(callback);
  };
 
+
+ /**
+   * @description readUserlabel function to read label
+   * @class  readUserlabel
+   * @extends {userId,callback}
+   */
+
+
  Label.prototype.readUserlabel = function (userId,callback) {
    //labelModel.readUserlabel(userId,callback);
   labelModel.find ({ user_id:userId}).sort({created_date: -1}).exec(callback);
  };
 
+ /**
+   * @description deleteUserlabel function to delete label
+   * @class  deleteUserlabel
+   * @extends {userId,paramId,callback}
+   */
+
  Label.prototype.deleteUserlabel = function (userId,paramId,callback) {
    //labelModel.deleteUserlabel(userId,paramId,callback);
    labelModel.remove({ user_id: userId,_id: paramId.id}).exec(callback);
  };
+
+ /**
+   * @description updateLabel function to update Label
+   * @class  updateLabel
+   * @extends {labelId,userId,requestbody,callback}
+   */
+
 Label.prototype.updateLabel = function (labelId,userId,requestbody,callback)
 {
   labelModel.findOneAndUpdate({_id: labelId,user_id:userId},requestbody,{new: true}).exec(callback);
--- service/todo.service.js
+++ service/todo.service.js
@@ -6,6 +6,12 @@ function Todo(){
 
 }
 
+/**
+  * @description createUserTodo function to create Todonotes..
+  * @class  createUserTodo
+  * @extends {todoObj,userObj,callback}
+  */
+
 Todo.prototype.createUserTodo = function (todoObj,userObj,callback) {
   var new_note = new TodoModel();
   new_note.title = todoObj.title;
@@ -18,31 +24,60 @@ Todo.prototype.createUserTodo = function (todoObj,userObj,callback) {
   new_note.save(callback);
 };
 
-
+/**
+  * @description readUserTodo function to read User Todonotes
+  * @class  readUserTodo
+  * @extends {userId,callback}
+  */
 Todo.prototype.readUserTodo = function (userId,callback) {
   TodoModel.find({ $or: [{user_id:userId},{collaborator:userId.email}]}).sort({created_date: -1}).limit(10).exec(callback);
 };
+
+/**
+  * @description readUserTodo function to read User Todonotes
+  * @class  deleteUserTodo
+  * @extends {userId,paramId,callback}
+  */
 
 Todo.prototype.deleteUserTodo = function (userId,paramId,callback) {
   TodoModel.remove({ user_id: userId, _id:paramId.id }).exec(callback);
 };
 
+/**
+  * @description updateUserTodo function to update User Todonotes
+  * @class  updateUserTodo
+  * @extends {userId,noteId,todoObj,callback}
+  */
  Todo.prototype.updateUserTodo = function(userId,noteId,todoObj,callback){
   TodoModel.findOneAndUpdate({ _id:noteId},todoObj,{new: true}).exec(callback);
  };
 
+ /**
+   * @description addupdateUserTodo function to add and update User Todonotes
+   * @class  updateUserTodo
+   * @extends {noteId,sharedNote,callback}
+   */
 Todo.prototype.addAndUpdateCollab =function(noteId,sharedNote,callback)
 {
  TodoModel.findOneAndUpdate({_id: noteId },{$addToSet:sharedNote},{new:true}).exec(callback);
 };
 
+/**
+  * @description deleteAndUpdateCollab function to delete and update User Todonotes
+  * @class  deleteAndUpdateCollab
+  * @extends {noteId,sharedNote,callback}
+  */
 Todo.prototype.deleteAndUpdateCollab =function(noteId,sharedNote,callback)
 {
  TodoModel.findOneAndUpdate({_id: noteId },{$pull:sharedNote},{new:true}).exec(callback);
 };
 
 
-
+/**
+  * @description searchTodos function to search User Todonotes
+  * @class  searchTodos
+  * @extends {userId,searchKey,callback}
+  */
 
 
   Todo.prototype.searchTodos = function (userId,searchKey,callback)
